c7af8cc89b7bf1869c5a7f49b7032ab03b33ca1e
aitor <aitorsancab@gmail.com>
2023-11-17 16:43:31 +0000

    mod login

---
+++ frontend/src/components/Logo/Logo.jsx
@@ -0,0 +1,14 @@
+import logo from "../../assets/rvr-logo.png";
+
+function Logo() {
+  return (
+    <>
+    <img src={logo} alt="logo"/>
+    <a>Reserve Room</a>
+    </>
+   
+
+  )
+}
+
+export default Logo
