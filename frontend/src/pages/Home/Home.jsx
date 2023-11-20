0d3601a2529839cc0e896b4e59662537e29d257d
yeray <yeray.programacion.web@gmail.com>
2023-11-20 15:29:47 +0000

    empezando carousel

--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import Header from '../../components/PaginaPrincipal/Header/Header'
+import CarrouselComponent from '../../components/PaginaPrincipal/Carousel/Carousel'
 
 function Home() {
   return (
     <>
     <Header/>
-    <div>Componente Carrousel</div>
+    <CarrouselComponent/>
     </>
   )
 }
