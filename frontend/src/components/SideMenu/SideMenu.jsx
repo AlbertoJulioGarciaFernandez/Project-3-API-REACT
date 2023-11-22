f200429cf99bdc70115c4b4ab6f57aaa25e76797
aitor <aitorsancab@gmail.com>
2023-11-22 14:25:18 +0000

    diseño layout

--- frontend/src/components/SideMenu/SideMenu.jsx
+++ frontend/src/components/SideMenu/SideMenu.jsx
@@ -1,14 +1,252 @@
-import { Link } from 'react-router-dom';
-import './SideMenu.css';
+import { Link } from "react-router-dom";
+import "./SideMenu.css";
+import {
+  Box,
+  Divider,
+  Drawer,
+  List,
+  ListItem,
+  ListItemButton,
+  ListItemIcon,
+  ListItemText,
+  Toolbar,
+} from "@mui/material";
+import { useState } from "react";
+
+import ListIcon from '@mui/icons-material/List';
+import AddIcon from '@mui/icons-material/Add';
+import ChangeCircleIcon from '@mui/icons-material/ChangeCircle';
+import DeleteIcon from '@mui/icons-material/Delete';
+
 
 function SideMenu() {
-    return (
-        <Link to={'/dashboard/listbookings'}>
-            <p>Ver reservas</p>
+  const [selectedIndex, setSelectedIndex] = useState(null);
+
+  const handleListItemClick = (index) => {
+    setSelectedIndex(index === selectedIndex ? null : index);
+  };
+  return (
+    
+    <Box position="absolute" className="menuDashboard">
+      <List className="btnMenu">
+        <Link to={`/dashboard/listbookings`} className="linkMenu">
+          <ListItem key="ver" disablePadding className="listItem">
+            <ListItemButton>
+              <ListItemIcon>
+                <ListIcon />
+              </ListItemIcon>
+              <ListItemText primary="Ver Reservas" />
+            </ListItemButton>
+          </ListItem>
         </Link>
-
-
-    )
+        <Link to={`/dashboard/listbookings`} className="linkMenu">
+          <ListItem key="ver" disablePadding className="listItem">
+            <ListItemButton>
+              <ListItemIcon>
+                <AddIcon></AddIcon>
+              </ListItemIcon>
+              <ListItemText primary="Crear Reserva" />
+            </ListItemButton>
+          </ListItem>
+        </Link>
+        <Link to={`/dashboard/listbookings`} className="linkMenu">
+          <ListItem key="ver" disablePadding className="listItem">
+            <ListItemButton>
+              <ListItemIcon>
+              <ChangeCircleIcon></ChangeCircleIcon>
+              </ListItemIcon>
+              <ListItemText primary="Modificar Reserva" />
+            </ListItemButton>
+          </ListItem>
+        </Link>
+        <Link to={`/dashboard/listbookings`} className="linkMenu">
+          <ListItem key="ver" disablePadding className="listItem">
+            <ListItemButton>
+              <ListItemIcon>
+              <DeleteIcon></DeleteIcon>
+              </ListItemIcon>
+              <ListItemText primary="Eliminar Reserva" />
+            </ListItemButton>
+          </ListItem>
+        </Link>
+      </List>
+      <Divider />
+      <List className="btnMenu">
+        <Link to={`/dashboard/listbookings`} className="linkMenu">
+          <ListItem key="ver" disablePadding className="listItem">
+            <ListItemButton>
+              <ListItemIcon>
+                <ListIcon />
+              </ListItemIcon>
+              <ListItemText primary="Ver Reservas" />
+            </ListItemButton>
+          </ListItem>
+        </Link>
+        <Link to={`/dashboard/listbookings`} className="linkMenu">
+          <ListItem key="ver" disablePadding className="listItem">
+            <ListItemButton>
+              <ListItemIcon>
+                <AddIcon></AddIcon>
+              </ListItemIcon>
+              <ListItemText primary="Crear Reserva" />
+            </ListItemButton>
+          </ListItem>
+        </Link>
+        <Link to={`/dashboard/listbookings`} className="linkMenu">
+          <ListItem key="ver" disablePadding className="listItem">
+            <ListItemButton>
+              <ListItemIcon>
+              <ChangeCircleIcon></ChangeCircleIcon>
+              </ListItemIcon>
+              <ListItemText primary="Modificar Reserva" />
+            </ListItemButton>
+          </ListItem>
+        </Link>
+        <Link to={`/dashboard/listbookings`} className="linkMenu">
+          <ListItem key="ver" disablePadding className="listItem">
+            <ListItemButton>
+              <ListItemIcon>
+              <DeleteIcon></DeleteIcon>
+              </ListItemIcon>
+              <ListItemText primary="Eliminar Reserva" />
+            </ListItemButton>
+          </ListItem>
+        </Link>
+      </List>
+      <Divider />
+      <List className="btnMenu">
+        <Link to={`/dashboard/listbookings`} className="linkMenu">
+          <ListItem key="ver" disablePadding className="listItem">
+            <ListItemButton>
+              <ListItemIcon>
+                <ListIcon />
+              </ListItemIcon>
+              <ListItemText primary="Ver Reservas" />
+            </ListItemButton>
+          </ListItem>
+        </Link>
+        <Link to={`/dashboard/listbookings`} className="linkMenu">
+          <ListItem key="ver" disablePadding className="listItem">
+            <ListItemButton>
+              <ListItemIcon>
+                <AddIcon></AddIcon>
+              </ListItemIcon>
+              <ListItemText primary="Crear Reserva" />
+            </ListItemButton>
+          </ListItem>
+        </Link>
+        <Link to={`/dashboard/listbookings`} className="linkMenu">
+          <ListItem key="ver" disablePadding className="listItem">
+            <ListItemButton>
+              <ListItemIcon>
+              <ChangeCircleIcon></ChangeCircleIcon>
+              </ListItemIcon>
+              <ListItemText primary="Modificar Reserva" />
+            </ListItemButton>
+          </ListItem>
+        </Link>
+        <Link to={`/dashboard/listbookings`} className="linkMenu">
+          <ListItem key="ver" disablePadding className="listItem">
+            <ListItemButton>
+              <ListItemIcon>
+              <DeleteIcon></DeleteIcon>
+              </ListItemIcon>
+              <ListItemText primary="Eliminar Reserva" />
+            </ListItemButton>
+          </ListItem>
+        </Link>
+      </List>
+      <Divider />
+      <List className="btnMenu">
+        <Link to={`/dashboard/listbookings`} className="linkMenu">
+          <ListItem key="ver" disablePadding className="listItem">
+            <ListItemButton>
+              <ListItemIcon>
+                <ListIcon />
+              </ListItemIcon>
+              <ListItemText primary="Ver Reservas" />
+            </ListItemButton>
+          </ListItem>
+        </Link>
+        <Link to={`/dashboard/listbookings`} className="linkMenu">
+          <ListItem key="ver" disablePadding className="listItem">
+            <ListItemButton>
+              <ListItemIcon>
+                <AddIcon></AddIcon>
+              </ListItemIcon>
+              <ListItemText primary="Crear Reserva" />
+            </ListItemButton>
+          </ListItem>
+        </Link>
+        <Link to={`/dashboard/listbookings`} className="linkMenu">
+          <ListItem key="ver" disablePadding className="listItem">
+            <ListItemButton>
+              <ListItemIcon>
+              <ChangeCircleIcon></ChangeCircleIcon>
+              </ListItemIcon>
+              <ListItemText primary="Modificar Reserva" />
+            </ListItemButton>
+          </ListItem>
+        </Link>
+        <Link to={`/dashboard/listbookings`} className="linkMenu">
+          <ListItem key="ver" disablePadding className="listItem">
+            <ListItemButton>
+              <ListItemIcon>
+              <DeleteIcon></DeleteIcon>
+              </ListItemIcon>
+              <ListItemText primary="Eliminar Reserva" />
+            </ListItemButton>
+          </ListItem>
+        </Link>
+      </List>
+      <Divider />
+      <List className="btnMenu">
+        <Link to={`/dashboard/listbookings`} className="linkMenu">
+          <ListItem key="ver" disablePadding className="listItem">
+            <ListItemButton>
+              <ListItemIcon>
+                <ListIcon />
+              </ListItemIcon>
+              <ListItemText primary="Ver Reservas" />
+            </ListItemButton>
+          </ListItem>
+        </Link>
+        <Link to={`/dashboard/listbookings`} className="linkMenu">
+          <ListItem key="ver" disablePadding className="listItem">
+            <ListItemButton>
+              <ListItemIcon>
+                <AddIcon></AddIcon>
+              </ListItemIcon>
+              <ListItemText primary="Crear Reserva" />
+            </ListItemButton>
+          </ListItem>
+        </Link>
+        <Link to={`/dashboard/listbookings`} className="linkMenu">
+          <ListItem key="ver" disablePadding className="listItem">
+            <ListItemButton>
+              <ListItemIcon>
+              <ChangeCircleIcon></ChangeCircleIcon>
+              </ListItemIcon>
+              <ListItemText primary="Modificar Reserva" />
+            </ListItemButton>
+          </ListItem>
+        </Link>
+        <Link to={`/dashboard/listbookings`} className="linkMenu">
+          <ListItem key="ver" disablePadding className="listItem">
+            <ListItemButton>
+              <ListItemIcon>
+              <DeleteIcon></DeleteIcon>
+              </ListItemIcon>
+              <ListItemText primary="Eliminar Reserva" />
+            </ListItemButton>
+          </ListItem>
+        </Link>
+      </List>
+  
+    </Box>
+    
+    
+    
+  );
 }
-
-export default SideMenu
+export default SideMenu;
--- frontend/src/layouts/index.jsx
+++ frontend/src/layouts/index.jsx
@@ -1,15 +1,18 @@
-import { Outlet } from 'react-router-dom'
-import Header from '../components/PaginaPrincipal/Header/Header'
-import SideMenu from '../components/SideMenu/SideMenu'
+import { Outlet } from "react-router-dom";
+import SideMenu from "../components/SideMenu/SideMenu";
+import HeaderDashboard from "../components/HeaderDashboard/HeaderDashboard";
+import "./index.css";
 
 function Root() {
   return (
-    <>
-      <Header />
-      <SideMenu />
-      <Outlet />
-    </>
-  )
+    <div className="bodyDashboard">
+      <div className="headerDashboard"><HeaderDashboard /></div>
+      <div className="cuerpo">
+        <div className="menuDashboard"><SideMenu /></div>
+        <div className="contenidoDashboard"><Outlet /></div>
+      </div>
+    </div>
+  );
 }
 
-export default Root
+export default Root;
