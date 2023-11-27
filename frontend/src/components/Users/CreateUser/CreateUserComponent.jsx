bd15cf436e4195ca03a1159092fc200f70df4ba5
yeray <yeray.programacion.web@gmail.com>
2023-11-27 11:57:26 +0000

    formulario altaUsuario terminado

--- frontend/src/components/Users/CreateUser/CreateUserComponent.jsx
+++ frontend/src/components/Users/CreateUser/CreateUserComponent.jsx
@@ -1,120 +1,5 @@
-/* import "./CreateUserComponent.css";
-import Box from "@mui/material/Box";
-import CardActions from "@mui/material/CardActions";
-import CardContent from "@mui/material/CardContent";
-import Button from "@mui/material/Button";
-import Typography from "@mui/material/Typography";
-import { TextField } from "@mui/material";
-import React from "react";
-
-function CreateUserComponent({handleChange, handleSubmit}){
-
-  const styles = {
-    texField: {
-      marginBottom: '20px'
-    }
-  }
-  
-  const card = (
-    <div className="containerFragment" >
-      <React.Fragment>
-
-        <Typography gutterBottom variant="h4" component="form">
-          Alta de Usuario
-        </Typography>
-
-        <CardContent className="formContainer">
-          
-          <TextField className="textField"  
-            sx={ styles.texField }
-            component="form"
-            required
-            id="outlined-required"
-            label="Nombre"
-            placeholder="Nombre"
-
-            onChange={handleChange}
-
-          />
-          <br />
-
-          <TextField className="textField"  
-            sx={ styles.texField }
-            component="form"
-            required
-            id="outlined-required"
-            label="Apellidos"
-            placeholder="Apellidos"
-
-            onChange={handleChange}
-          />
-          <br />
-
-          <TextField className="textField"
-           sx={ styles.texField }
-            component="form"
-            required
-            id="outlined-required"
-            label="Direccion"
-            placeholder="Direccion"
-          
-            onChange={handleChange}
-          />
-          <br />
-
-          <TextField className="textField"
-           sx={ styles.texField }
-            component="form"
-            required
-            id="outlined-required"
-            label="Email"
-            placeholder="Email"
-
-            onChange={handleChange}
-          />
-          <br />
-
-          <TextField className="textField"
-           sx={ styles.texField }
-            id="outlined-password-input"
-            label="Password"
-            type="Contraseña"
-            placeholder="Contraseña"
-            autoComplete="current-password"
-
-            onChange={handleChange}
-          />
-        </CardContent>
-        <CardActions>
-          <Button 
-          type="submit" 
-          variant="contained" 
-          color="success" 
-          onSubmit={handleSubmit}>
-            Enviar
-          </Button>
-        </CardActions>
-      </React.Fragment>
-    </div>
-  );
-
-  return (
-    <>
-      <Box sx={{ minWidth: 275 }}>
-        <div>{card}</div>
-      </Box>
-    </>
-  );
-}
-
-export default CreateUserComponent;
- */
-
-//---------------------------------------------------------------
-
 // CreateUserComponent.jsx
 import "./CreateUserComponent.css";
-import Box from "@mui/material/Box";
 import CardActions from "@mui/material/CardActions";
 import CardContent from "@mui/material/CardContent";
 import Button from "@mui/material/Button";
@@ -123,19 +8,10 @@ import { TextField } from "@mui/material";
 import React  from "react";
 
 export default function CreateUserComponent({ handleSubmit, setUser, user }) {
-  // Estados locales para cada campo del formulario
- /*  const [nombre, setNombre] = useState("");
-  const [apellidos, setApellidos] = useState("");
-  const [direccion, setDireccion] = useState("");
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const [newUser, setNewUser] = useState({});
- */
-
 
   return (
     <div className="containerFragment">
-      <form>
+      <form className="formulario">
         <React.Fragment>
           <Typography gutterBottom variant="h4" component="form">
             Alta de Usuario
@@ -205,7 +81,7 @@ export default function CreateUserComponent({ handleSubmit, setUser, user }) {
             />
           </CardContent>
 
-          <CardActions>
+          <CardActions className="botonEnviar">
             <Button variant="contained" color="success" onClick={handleSubmit}>
               Enviar
             </Button>
--- frontend/src/pages/Users/CreateUser/CreateUserPage.jsx
+++ frontend/src/pages/Users/CreateUser/CreateUserPage.jsx
@@ -1,63 +1,3 @@
-/* import CreateUserComponent from '../../../components/Users/CreateUser/CreateUserComponent'
-
-import { useState } from 'react';
-
-function CreateUserPage() {
-
-  const [user, setUser] = useState({
-    nombre: "",
-    apellidos: "",
-    direccion: "",
-    email: "",
-    contraseña: "",
-  });
-  
-  const handleChange = (e) => {
-    const { name, value } = e.target;
-  
-    setUser((usuarioRegistrado) => ({
-      ...usuarioRegistrado,
-      [name]: value,
-    }));
-  };
-  
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-
-    try {
-      // Realizar la solicitud POST al backend para guardar el nuevo usuario
-      const response = await fetch('/api/user', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(user),
-      });
-
-      if (response.ok) {
-        console.log('Usuario creado exitosamente');
-        
-      } else {
-        console.error('Error al crear el usuario');
-      }
-    } catch (error) {
-      console.error('Error en la solicitud:', error);
-    }
-  };
-
-
-  return (
-    <CreateUserComponent  handleSubmit= {handleSubmit} handleChange={handleChange}/>
-  )
-}
-
-CreateUserPage.propTypes = {}
-
-export default CreateUserPage
- */
-
-//---------------------------------------------------------------
-
 // CreateUserPage.jsx
 import CreateUserComponent from "../../../components/Users/CreateUser/CreateUserComponent";
 import { useState } from "react";
@@ -71,13 +11,18 @@ function CreateUserPage() {
     email: "",
     password: "",
   });
- 
-  const handleSubmit = async () => {
-    const response = await createUser(user)
 
+  const handleSubmit = async () => {
+    const response = await createUser(user);
   };
 
-  return <CreateUserComponent handleSubmit={handleSubmit} setUser={setUser} user={user} />;
+  return (
+    <CreateUserComponent
+      handleSubmit={handleSubmit}
+      setUser={setUser}
+      user={user}
+    />
+  );
 }
 
 export default CreateUserPage;
